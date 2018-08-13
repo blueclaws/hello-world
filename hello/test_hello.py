2ee21a2fe551e8c3f2e293e302fa73c85e2232f9
Claw <blueclaws@blueclaws.in>
2018-08-13 14:17:43 +0530

    FU

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -7,22 +7,11 @@ from hello import hello
 class TestHello(unittest.TestCase):
     """sample test"""
 
-    def test_world(self):
+    def test_three(self):
         """sample test"""
         self.assertEqual(3, 3)
 
-    def test_world_unicode(self):
+    def test_four(self):
         """sample test with unicode"""
         self.assertEqual(4, 4)
 
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestHello('test_div'))
-    #suite.addTest(TestHello('test_func'))
-    #suite.addTest(TestHello('test_imlol'))
-    return suite
-
-if __name__ == '__main__':
-    #unittest.main()
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
